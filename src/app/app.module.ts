f0721ac30aec6ad4adc144f04046eacc206990ee
Ajinkya Ladkhedkar <ajinkyaladkhedkar@gmail.com>
2018-02-23 13:21:31 -0800

    feat(module: user-details): add static user-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular//common/http';
 
 import { AppComponent } from './app.component';
 import {VehicleInfoService} from './vehicle-info-service.service';
 import { VehicleFormComponentComponent } from './vehicle-form-component/vehicle-form-component.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    VehicleFormComponentComponent
+    VehicleFormComponentComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [VehicleInfoService],   //Register Services at Application level scope
   bootstrap: [AppComponent]
